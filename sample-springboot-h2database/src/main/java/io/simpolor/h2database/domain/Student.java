a0b568e5ac29253d8406f9de96a1e270b5fb1ab6
parksy <parksy@smilegate.com>
2019-10-07 15:55:04 +0900

    [MOD] h2database : UUID 방법 추가

--- sample-springboot-h2database/src/main/java/io/simpolor/h2database/domain/Student.java
+++ sample-springboot-h2database/src/main/java/io/simpolor/h2database/domain/Student.java
@@ -1,6 +1,7 @@
 package io.simpolor.h2database.domain;
 
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 
@@ -9,8 +10,16 @@ import javax.persistence.*;
 public class Student {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long seq;
+
+	/* @Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(
+			name = "UUID",
+			strategy = "org.hibernate.id.UUIDGenerator"
+	)
+	private long id; */
 
 	private String name;
 	private int grade;
