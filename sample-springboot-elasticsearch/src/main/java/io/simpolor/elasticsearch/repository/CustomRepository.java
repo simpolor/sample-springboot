41a396cea1e20fe784a791f4aedb9e30abbb6aa8
parksy <parksy@smilegate.com>
2019-11-01 15:47:04 +0900

    sample-springboot-elasticsearch, sample-springboot-mongo, sample-springboot-redis 이관에 따른 모듈 삭제

--- sample-springboot-elasticsearch/src/main/java/io/simpolor/elasticsearch/repository/CustomRepository.java
+++
@@ -1,9 +0,0 @@
-package io.simpolor.elasticsearch.repository;
-
-import io.simpolor.elasticsearch.domain.Student;
-
-public interface CustomRepository {
-
-    Student update(Student student);
-
-}
--- sample-springboot-mongo/src/main/java/io/simpolor/mongo/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package io.simpolor.mongo.repository;
-
-import io.simpolor.mongo.domain.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface StudentRepository extends MongoRepository<Student, String> {
-
-}
