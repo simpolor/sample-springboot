c94e0710c3b0a1357af77670506b6a1bca8b1b69
Simpolor <simpolor@gmail.com>
2019-11-02 21:50:01 +0900

    sample-springboot-io 이관으로 인한 관련 모듈 삭제

--- sample-springboot-batch/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO student(name, grade, age, reg_date)
-     VALUES ('하니', 1, 17, '2019-07-22 16:08:00');
-
-INSERT INTO student(name, grade, age, reg_date)
-     VALUES ('홍길동', 3, 19, '2019-08-01 15:45:00');
