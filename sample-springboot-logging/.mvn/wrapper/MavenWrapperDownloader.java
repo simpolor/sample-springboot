62f6864538bba522047ed0c9fc4eb34286d83fe4
parksy <parksy@smilegate.com>
2019-12-31 16:07:55 +0900

    전체 : 로그백 추가 및 다운로드 추가

--- sample-springboot-logging/.mvn/wrapper/MavenWrapperDownloader.java
+++ sample-springboot-logging/.mvn/wrapper/MavenWrapperDownloader.java
@@ -29,7 +29,7 @@ public class MavenWrapperDownloader {
             + WRAPPER_VERSION + "/maven-wrapper-" + WRAPPER_VERSION + ".jar";
 
     /**
-     * Path to the maven-wrapper.properties file, which might contain a downloadUrl property to
+     * Path to the maven-wrapper.properties file, which might contain a downloadUrl config to
      * use instead of the default one.
      */
     private static final String MAVEN_WRAPPER_PROPERTIES_PATH =
@@ -42,7 +42,7 @@ public class MavenWrapperDownloader {
             ".mvn/wrapper/maven-wrapper.jar";
 
     /**
-     * Name of the property which should be used to override the default download url for the wrapper.
+     * Name of the config which should be used to override the default download url for the wrapper.
      */
     private static final String PROPERTY_NAME_WRAPPER_URL = "wrapperUrl";
 
