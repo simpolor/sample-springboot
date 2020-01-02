a29f81a5477095caa9a10fe711c2412fde8099cf
parksy <parksy@smilegate.com>
2020-01-02 17:31:56 +0900

    samplep-springbot-property : Config에 validator 추가

--- sample-springboot-property/src/main/java/io/simpolor/property/component/DatabaseProperty.java
+++ sample-springboot-property/src/main/java/io/simpolor/property/component/DatabaseProperty.java
@@ -1,21 +1,25 @@
 package io.simpolor.property.component;
 
 import lombok.Getter;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 @Getter
 @Component
 @PropertySource("classpath:database.yml")
 @ConfigurationProperties(prefix = "database")
+@Validated
 public class DatabaseProperty {
 
     @Value("${host}")
     private String host;
 
+    @Length(min = 5, max = 10)
     @Value("${port}")
     private String port;
 
